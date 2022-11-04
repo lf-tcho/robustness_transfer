4554974367cd4747cd09d90a9f42c607d66215db
Maurice Georgi <test>
2022-11-04 18:57:04 +0100

    Add eval logic

--- dataloader.py
+++ dataloader.py
@@ -1,32 +1,41 @@
 import torchvision.datasets as datasets
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torchvision import transforms
+import torch
 
 
 DATA_DIR = "./data"
 
 
-def get_dataset(dataset_name: str, train: bool = False):
+def get_dataset(dataset_name: str, train: bool = False, size: int = None):
     """Load pytorch dataset.
 
     :param name: Name of dataset
     :param train: If True loads train set, else test set, default False
+    :param size: Takes only first size number of items of dataset
     """
     if dataset_name == "cifar10":
-        return datasets.CIFAR10(
+        dataset = datasets.CIFAR10(
             root=DATA_DIR,
             train=train,
             download=True,
             transform=transforms.Compose([transforms.ToTensor()]),
         )
+    if size:
+        indices = torch.arange(size)
+        dataset = Subset(dataset, indices)
+    return dataset
 
 
-def get_dataloader(dataset_name: str, train: bool = False, batch_size: int = 1):
+def get_dataloader(
+    dataset_name: str, train: bool = False, batch_size: int = 1, size: int = None
+):
     """Get dataloader for given dataset name.
 
     :param name: Name of dataset
     :param train: If True loads train set, else test set, default False
     :param batch_size: Batch size of dataloader
+    :param size: Takes only first size number of items of dataset
     """
-    dataset = get_dataset(dataset_name, train)
+    dataset = get_dataset(dataset_name, train, size)
     return DataLoader(dataset, batch_size)
--- train.py
+++ train.py
@@ -2,11 +2,11 @@ from dataloader import get_dataloader
 from robustbench.utils import load_model
 import torch.optim as optim
 import torch.nn as nn
+import torch
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
 from pathlib import Path
-
-LOG_DIR = "./log"
+import numpy as np
 
 
 class Trainer:
@@ -15,49 +15,69 @@ class Trainer:
     def __init__(
         self,
         model,
-        dataloader,
+        train_dataloader,
+        eval_dataloader,
         loss,
         epochs,
+        optimizer,
         experiment_name: str = "test",
-        optimizer: str = "sgd",
     ):
         self.model = model
-        self.dataloader = dataloader
+        self.train_dataloader = train_dataloader
+        self.eval_dataloader = eval_dataloader
         self.loss = loss
         self.epochs = epochs
         self.optimizer = optimizer
-        self.log_dir = Path(LOG_DIR) / experiment_name
-
-    def get_optimizer(self):
-        """Get optimizer."""
-        if self.optimizer == "sgd":
-            return optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
+        self.experiment_folder = Path("./experiments") / experiment_name
 
     def train(self):
         """Train network."""
-        writer = SummaryWriter(log_dir=self.log_dir)
-        optimizer = self.get_optimizer()
+        writer = SummaryWriter(log_dir=self.experiment_folder / "logs")
+        optimizer = self.optimizer
         iteration = 0
         for epoch in range(self.epochs):
             print(f"Epoch: {epoch}")
-            for batch in tqdm(self.dataloader):
-                inputs, labels = batch
+            # Train one epoch
+            for inputs, labels in tqdm(self.train_dataloader):
+                iteration += self.train_dataloader.batch_size
                 optimizer.zero_grad()
-                outputs = self.model(inputs)
-                loss = self.loss(outputs, labels)
+                metrics = self.step(inputs, labels)
+                loss = metrics["loss"]
                 loss.backward()
                 optimizer.step()
                 writer.add_scalar("Loss/train", loss.item(), iteration)
-                iteration += self.dataloader.batch_size
+
+            # Evaluate model
+            losses = []
+            with torch.no_grad():
+                for inputs, labels in tqdm(self.eval_dataloader):
+                    metrics = self.step(inputs, labels)
+                    losses.append(metrics["loss"].item())
+            loss = np.mean(losses)
+            writer.add_scalar("Loss/val", loss, iteration)
+        self.save_model(f"ckpt{epoch}")
+
+    def step(self, inputs, labels):
+        """Do one batch step."""
+        metrics = {}
+        model_output = self.model(inputs)
+        metrics["loss"] = self.loss(model_output, labels)
+        return metrics
+
+    def save_model(self, name):
+        """Save model."""
+        torch.save(self.model.state_dict(), self.experiment_folder / f"{name}.pth")
 
 
 def main():
     model = load_model(
         model_name="Carmon2019Unlabeled", dataset="cifar10", threat_model="Linf"
     )
-    dataloader = get_dataloader("cifar10", False, batch_size=10)
+    train_dataloader = get_dataloader("cifar10", True, batch_size=10, size=100)
+    eval_dataloader = get_dataloader("cifar10", False, batch_size=10, size=50)
     loss = nn.CrossEntropyLoss()
-    trainer = Trainer(model, dataloader, loss, 10)
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    trainer = Trainer(model, train_dataloader, eval_dataloader, loss, 10, optimizer)
     trainer.train()
 
 
