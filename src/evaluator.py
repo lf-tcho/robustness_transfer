433b1c053d75a927e88c17bbba00f24a2043cdc3
Maurice Georgi <test>
2022-11-06 11:57:02 +0100

    Add accurarcy and robust accuracy (L inf PDG) to evaluator

--- src/evaluator.py
+++ src/evaluator.py
@@ -5,10 +5,11 @@ from src.trainer import CKPT_NAME
 import torch
 from pathlib import Path
 import foolbox as fb
+from tqdm import tqdm
 
 
 class Evaluator:
-    def __init__(self, experiment: Experiment, epoch: int, dataloader) -> None:
+    def __init__(self, experiment: Experiment, dataloader, epoch: int = None) -> None:
         self.experiment = experiment
         self.epoch = epoch
         self.dataloader = dataloader
@@ -16,30 +17,40 @@ class Evaluator:
     def eval(self):
         """Run evaluation for an experiment."""
         model = self.load_model()
-        fmodel = fb.PyTorchModel(model, bounds=(0, 1))
-        for inputs, labels in self.dataloader:
-            _, advs, success = fb.attacks.LinfPGD()(
-                fmodel, inputs, labels, epsilons=[8 / 255]
-            )
-            print("Robust accuracy: {:.1%}".format(1 - success.float().mean()))
+        fmodel = fb.PyTorchModel(model, bounds=(-1, 1))
+        accuracy = 0
+        robust_accuracy = 0
+        l_inf_pgd = fb.attacks.LinfPGD(steps=20)
+        for inputs, labels in tqdm(self.dataloader):
+            batch_size = inputs.shape[0]
+            accuracy += fb.utils.accuracy(fmodel, inputs, labels) * batch_size
+            _, _, success = l_inf_pgd(fmodel, inputs, labels, epsilons=[8 / 255])
+            robust_accuracy += batch_size - success.float().sum().item()
+            # print("Robust accuracy: {:.1%}".format(1 - success.float().mean()))
+        accuracy = accuracy / len(self.dataloader.dataset)
+        print(f"Accurarcy: {accuracy}")
+        robust_accuracy = robust_accuracy / len(self.dataloader.dataset)
+        print(f"Robust accuracy: {robust_accuracy}")
 
     def load_model(self):
         model = self.experiment.get_model()
-        model.load_state_dict(
-            torch.load(
-                Path("./experiments")
-                / self.experiment.experiment_name
-                / f"{CKPT_NAME}_{self.epoch}.pth"
+        if self.epoch:
+            experiment_folder = Path("./experiments") / self.experiment.experiment_name
+            model.load_state_dict(
+                torch.load(experiment_folder / f"{CKPT_NAME}_{self.epoch}.pth")
             )
-        )
-        return model
+            return model
+        else:
+            return model
 
 
 if __name__ == "__main__":
-    from experiments_configs.test_experiment import TestExperiment
+    from experiments_configs.lp_ft_experiment import LpFtExperiment
     from src.dataloader import get_dataloader
+    from torchvision.transforms import Normalize
 
-    experiment = TestExperiment("test")
-    dataloader = get_dataloader("cifar10", False, batch_size=1, size=5)
-    evaluator = Evaluator(experiment, 4, dataloader)
+    experiment = LpFtExperiment("lp_ft")
+    transforms = [Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    dataloader = get_dataloader("cifar100", False, 5, 50, transforms)
+    evaluator = Evaluator(experiment, dataloader)
     evaluator.eval()
