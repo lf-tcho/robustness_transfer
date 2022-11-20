fb4a6b33c2d55f44ac1e4c92d900562bee3a8a2c
Maurice Georgi <test>
2022-11-20 13:54:50 +0100

    Log learning rate

--- src/trainer.py
+++ src/trainer.py
@@ -75,7 +75,10 @@ class Trainer:
                 optimizer.step()
                 writer.add_scalar("Loss/train", loss.item(), iteration)
             if self.lr_scheduler:
+                writer.add_scalar("Parameter/LR", self.lr_scheduler.get_last_lr(), iteration)
                 self.lr_scheduler.step()
+            else:
+                writer.add_scalar("Parameter/LR", optimizer.param_groups[0]['lr'], iteration)
 
             # Evaluate model
             self.model.eval()
