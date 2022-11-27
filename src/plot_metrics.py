435a4d9c0e69d7016fd62dee10f18ea2cb398e44
Maurice Georgi <test>
2022-11-27 12:08:09 +0100

    Plot metric jsons

--- src/plot_metrics.py
+++ src/plot_metrics.py
@@ -1,21 +1,42 @@
-from numpy import genfromtxt
+import argparse
 import matplotlib.pyplot as plt
 from pathlib import Path
+import json
 
-path_lp = Path(
-    r"C:\Users\mauri\Documents\Stanford\dmtml\project\dmtml\experiments\lp_bs_128_eps_10_lr_0.001\logs\lp_bs_128_eps_10_lr_0.001_logs.csv"
-)
-path_ff = Path(
-    r"C:\Users\mauri\Documents\Stanford\dmtml\project\dmtml\experiments\lp_bs_128_eps_10_lr_0.001_lp_0\logs\lp_bs_128_eps_10_lr_0.001_lp_0_logs.csv"
-)
-ff = genfromtxt(path_ff, delimiter=",", names=True)
-lp = genfromtxt(path_lp, delimiter=",", names=True)
 
-fig, ax = plt.subplots()
-ax.plot(ff["Step"], ff["Value"], marker="o", label="Full fine-tuning")
-ax.set_xlabel("Iteration")
-ax.set_ylabel("Test accuracy")
-ax.plot(lp["Step"], lp["Value"], marker="o", label="Linear probing")
-ax.legend(loc="center right")
-plt.show()
-plt.savefig('test_accuracy.png')
+def plot_metrics(experiment_path: Path):
+    """Plot accuracy and robust accuracy of metric jsons.
+
+    :param experiment_path: Path to experiment
+    """
+    metric_files = sorted(list(experiment_path.glob("*.json")))
+    accuracy = []
+    robust_accuracy = []
+    epochs = []
+    for metric_file in metric_files:
+        num = ''.join(i for i in metric_file.stem if i.isdigit())
+        if len(num) > 0:
+            num = int(num)
+            with open(metric_file, "r") as file:
+                metrics = json.load(file)
+            accuracy.append(metrics["accuracy"])
+            robust_accuracy.append(metrics["robust_accuracy"])
+            epochs.append(num)
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax1.plot(epochs, accuracy, 'g-')
+    ax2.plot(epochs, robust_accuracy, 'b-')
+
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Accuracy', color='g')
+    ax2.set_ylabel('Robust accuracy', color='b')
+    ax1.set_title(experiment_path.name)
+    fig.savefig(experiment_path / "metrics.png")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-folder", "--folder", default=".", type=str)
+    args = parser.parse_args()
+    experiment_path = Path(args.folder)
+    plot_metrics(experiment_path)
